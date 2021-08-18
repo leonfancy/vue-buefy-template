ad0d13897d2142f98a0876380925d834d00723ea
Chen Liang <slimhigh@gmail.com>
2021-08-18 21:57:36 +0800

    Add vuex.

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ Vue.config.productionTip = false
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import router from './router'
+import store from './store'
 
 Vue.use(Buefy, { defaultIconPack: 'fas' })
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
