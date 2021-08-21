8d95b8f253dbc235a08307bf355895609f52479c
Chen Liang <slimhigh@gmail.com>
2021-08-21 15:33:19 +0800

    Add Login page.

--- src/components/VFooter.vue
+++ src/components/VFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer has-background-light">
     <div class="content has-text-centered">
-      <p>2021 &copy; <a href="https://chenliang.org">Chen Liang</a></p>
+      <p>2021 &copy; <a href="https://chenliang.org" target="_blank">Chen Liang</a></p>
       <p>
         <strong>Vue Template</strong> is built with <a href="https://buefy.org/" target="_blank">Buefy</a>,
         <a href="https://bulma.io" target="_blank">Bulma</a>, and <a href="https://vuejs.org" target="_blank">Vue.js</a>.
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="hero is-light is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-8-tablet is-offset-2-tablet is-6-desktop is-offset-3-desktop is-4-widescreen is-offset-4-widescreen">
+            <login-form></login-form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import LoginForm from "@/components/LoginForm";
+
+export default {
+  name: "Login",
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
