d63805c8c7253e855324c40085d7f38ac68c9c5e
Chen Liang <slimhigh@gmail.com>
2021-08-21 20:37:02 +0800

    Add logo and title to login page.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,6 +4,13 @@
       <div class="container">
         <div class="columns">
           <div class="column is-8-tablet is-offset-2-tablet is-6-desktop is-offset-3-desktop is-4-widescreen is-offset-4-widescreen">
+            <div class="block is-flex is-align-items-center is-justify-content-center">
+              <figure class="image is-32x32 mr-3">
+                <img src="../assets/logo.png"/>
+              </figure>
+              <div class="has-text-weight-bold is-size-5 has-text-primary">Vue Template</div>
+            </div>
+            <div class="block has-text-centered title">Login to start</div>
             <login-form></login-form>
           </div>
         </div>
