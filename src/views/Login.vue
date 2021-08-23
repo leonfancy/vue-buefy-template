daa07822469fabb4e642f4061c8c3d1ea8823e84
Liang Chen <slimhigh@gmail.com>
2021-08-23 12:43:56 +0800

    Fix login page style.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,19 +1,21 @@
 <template>
   <section class="hero is-light is-fullheight">
     <div class="hero-body">
-      <div class="container columns">
-        <div class="column is-8-tablet is-offset-2-tablet is-6-desktop is-offset-3-desktop is-4-widescreen is-offset-4-widescreen">
-          <router-link to="/" class="is-block block is-flex is-align-items-center is-justify-content-center">
-            <figure class="image is-32x32 mr-3">
-              <img src="../assets/logo.png"/>
-            </figure>
-            <div class="has-text-weight-bold is-size-5 has-text-primary">Vue Template</div>
-          </router-link>
-          <div class="block has-text-centered is-size-5">Login to start</div>
-          <login-form></login-form>
-          <p>Don't have an account?
-            <router-link to="/signup" class="has-text-primary">Sign up</router-link>
-          </p>
+      <div class="container">
+        <div class="columns">
+          <div class="column is-8-tablet is-offset-2-tablet is-6-desktop is-offset-3-desktop is-4-widescreen is-offset-4-widescreen">
+            <router-link to="/" class="is-block block is-flex is-align-items-center is-justify-content-center">
+              <figure class="image is-32x32 mr-3">
+                <img src="../assets/logo.png"/>
+              </figure>
+              <div class="has-text-weight-bold is-size-5 has-text-primary">Vue Template</div>
+            </router-link>
+            <div class="block has-text-centered is-size-5">Login to start</div>
+            <login-form></login-form>
+            <p>Don't have an account?
+              <router-link to="/signup" class="has-text-primary">Sign up</router-link>
+            </p>
+          </div>
         </div>
       </div>
     </div>
