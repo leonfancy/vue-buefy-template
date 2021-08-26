438f0ce13033792de0bc06b163967c76b4ba480c
Chen Liang <slimhigh@gmail.com>
2021-08-27 05:31:47 +0800

    Add route guard.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from '@/store'
 import Home from '@/views/Home.vue'
 import Login from "@/views/Login";
 import Signup from "@/views/Signup";
@@ -11,22 +12,34 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      visibility: "ONLY_NO_LOGIN"
+    }
   },
   {
     path: '/user',
     name: 'UserHome',
-    component: UserHome
+    component: UserHome,
+    meta: {
+      visibility: "ONLY_LOGIN"
+    }
   },
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: {
+      visibility: "ONLY_NO_LOGIN"
+    }
   },
   {
     path: '/signup',
     name: 'Signup',
-    component: Signup
+    component: Signup,
+    meta: {
+      visibility: "ONLY_NO_LOGIN"
+    }
   },
   {
     path: '/about',
@@ -34,12 +47,25 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: {
+      visibility: "ANY"
+    }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.visibility === "ONLY_NO_LOGIN") && store.state.auth.isLoggedIn) {
+    next({name: "UserHome"})
+  } else if (to.matched.some(record => record.meta.visibility === "ONLY_LOGIN") && !store.state.auth.isLoggedIn) {
+    next({name: "Login"})
+  } else {
+    next()
+  }
 })
 
 export default router
